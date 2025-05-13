f7edb1f1da4c90df958f0a82d9da628eebe4099d
KolodnikovVyacheslav <slava@kolodnikov.com>
2025-05-13 23:27:31 +0300

    Напсиал main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import CSV.CSVImporter;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVParserBuilder;
+import data.Department;
+import data.Employee;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            List<Employee> staff = CSVImporter.loadEmployees("foreign_names.csv", ';');
+            for (Employee emp : staff) {
+                System.out.println(emp);
+            }
+        } catch (Exception ex) {
+            System.err.println("Ошибка при обработке: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
